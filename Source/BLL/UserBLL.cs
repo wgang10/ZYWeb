489f30205877e906acdcf7ece2d8a13556c37bc7
wangang <wgang10@foxmail.com>
2012-07-18 18:21:51 +0800

    添加VS功能，测试数据库

---
+++ Source/BLL/UserBLL.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ZYSoft.SQLDAL;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ZYSoft.BLL
+{
+    public class UserBLL
+    {
+        public string GetUaerName()
+        {
+            string SQL = @"Select Top 1 Name From TUser";
+            DataTable tb = new DataTable();
+            using (SqlHelper helper = new SqlHelper())
+            {
+                try
+                {
+                    List<SqlParameter> parameters = new List<SqlParameter>();
+                    tb = helper.ExecuteDataTable(SQL, CommandType.Text, parameters);
+                    if (tb.Rows.Count > 0)
+                    {
+                        return tb.Rows[0][0].ToString();
+                    }
+                    else
+                    {
+                        return "没有数据！";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return ex.Message;
+                }
+            }
+        }
+    }
+}
---
+++ Source/Web/Default.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ZYSoft.BLL;
+
+namespace Web
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        private readonly UserBLL bll;
+
+        public Default()
+        {
+            bll = new UserBLL();
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                this.lbMessage.Text = GetData()+ " 获取时间 "+DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            }
+        }
+
+        private string GetData()
+        {
+            return bll.GetUaerName();
+        }
+    }
+}
