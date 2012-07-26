deb61986303ca694039eb805f37292300e430f3a
wangang <wgang10@foxmail.com>
2012-07-27 00:31:29 +0800

    修改QQ登陆

--- Source/Web/Default.aspx
+++ Source/Web/Default.aspx
@@ -17,15 +17,41 @@
     <form id="form1" runat="server">
     <p><strong>子杨软件</strong></p>
     <p>Zi Yang Soft</p>
+    <div>
+    <style>
+.bizmail_loginpanel{font-size:12px;width:300px;height:auto;border:1px solid #cccccc;background:#ffffff;}
+.bizmail_LoginBox{padding:10px 15px;}
+.bizmail_loginpanel h3{padding-bottom:5px;margin:0 0 5px 0;border-bottom:1px solid #cccccc;font-size:14px;}
+.bizmail_loginpanel form{margin:0;padding:0;}
+.bizmail_loginpanel input.text{font-size:12px;width:100px;height:20px;margin:0 2px;border:1px solid #C3C3C3;border-color:#7C7C7C #C3C3C3 #C3C3C3 #9A9A9A;}
+.bizmail_loginpanel .bizmail_column{height:28px;}
+.bizmail_loginpanel .bizmail_column label{display:block;float:left;width:30px;height:24px;line-height:24px;font-size:12px;}
+.bizmail_loginpanel .bizmail_column .bizmail_inputArea{float:left;width:240px;}
+.bizmail_loginpanel .bizmail_column span{font-size:12px;word-wrap:break-word;margin-left: 2px;line-height:200%;}
+.bizmail_loginpanel .bizmail_SubmitArea{margin-left:30px;clear:both;}
+.bizmail_loginpanel .bizmail_SubmitArea a{font-size:12px;margin-left:5px;}
+.bizmail_loginpanel select{width:110px;height:20px;margin:0 2px;}
+</style>
+<script type="text/javascript" src="http://exmail.qq.com/zh_CN/htmledition/js_biz/outerlogin.js"  charset="gb18030"></script>
+<script type="text/javascript">
+    writeLoginPanel({ domainlist: "ziyangsoft.com", mode: "vertical" });
+</script>
+</div>
     <p>
         <asp:Label ID="lbMessage" runat="server"></asp:Label>
     </p>
-                <div class="login-r Fixed" runat="server">                    
-                    <ul class="login-list">
+                <div runat="server">                    
+                    <ul>
                         <li>
                             <div class="account">
-                                <input type="image" src="../images/Connect_logo_4.png" name="btQQLogin" value="腾讯QQ登录"
-                                    id="Image1" onclick="openQQ();return false;" />
+                                <img src="./images/Connect_logo_4.png" alt="使用腾讯QQ登录" onclick="openQQ();return false;" />
+                            </div>
+                        </li>
+                        <li>
+                            <div>
+		                        <a href="https://graph.qq.com/oauth2.0/authorize?response_type=code&client_id=100289171&redirect_uri=www.ziyangsoft.com&scope=get_user_info">
+			                        <img alt="使用腾讯QQ登陆" src="./images/Connect_logo_5.png" />
+		                        </a>
                             </div>
                         </li>
                     </ul>
--- Source/Web/Default.aspx.cs
+++ Source/Web/Default.aspx.cs
@@ -6,6 +6,7 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using ZYSoft.BLL;
 using ZYSoft.Comm.Entity;
+using System.Configuration;
 
 namespace Web
 {
@@ -22,10 +23,71 @@ namespace Web
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (!IsPostBack)
+            //if (!IsPostBack)
+            //{
+            //    this.lbMessage.Text = GetData()+ " 获取时间 "+DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            //    BindGridData();
+            //}
+            GetUserInfo();
+        }
+
+        private void GetUserInfo()
+        {
+            if (Request.QueryString["oauth_vericode"] != null)
             {
-                this.lbMessage.Text = GetData()+ " 获取时间 "+DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
-                BindGridData();
+                var requestTokenKey = Session["requesttokenkey"].ToString();
+                var requestTokenSecret = Session["requesttokensecret"].ToString();
+                var verifier = Request.QueryString["oauth_vericode"];
+                string key = ConfigurationManager.AppSettings["ConsumerKey"];
+                string secret = ConfigurationManager.AppSettings["ConsumerSecret"];
+                QzoneSDK.Qzone qzone = new QzoneSDK.Qzone(key, secret, requestTokenKey, requestTokenSecret, verifier);
+
+                //这里需要将qzone.OAuthTokenKey, qzone.OAuthTokenSecret, qzone.OpenID 存储起来用于后面的API的访问
+                QzoneSDK.Qzone qzone2 = new QzoneSDK.Qzone(key, secret, qzone.OAuthTokenKey, qzone.OAuthTokenSecret, string.Empty, true, qzone.OpenID);
+                Session["qzonesdk"] = qzone2;
+
+                //qzone2 = Session["qzonesdk"] as QzoneSDK.Qzone;
+                var currentUser = qzone2.GetCurrentUser();
+                this.lbMessage.Text = currentUser;
+                //var user = (BasicProfile)JsonConvert.Import(typeof(BasicProfile), currentUser);
+                //if (null != currentUser)
+                //{
+                //this.result.Text = "成功登陆";
+                //this.Nickname.Text = user.Nickname;
+                //this.Figureurl.Text = user.Figureurl;
+                //this.lbMoreInfo.Text="User's Msg:"+user.Msg+"User's Ret:"+user.Ret.ToString();
+                //Image1.ImageUrl=user.Figureurl;
+                //var list = Database.Instance.QzoneOauth.Where(x => x.OpenId == qzone2.OpenID).ToList();
+                //if (list.Count > 0)
+                //{
+                //    QzoneOauth model = list[0];
+                //    var newCookie = new HttpCookie("MemberID", model.UserId.ToString());
+                //    newCookie.Expires = DateTime.Now.AddDays(10);
+                //    Response.AppendCookie(newCookie);
+                //    Session["QzoneOauth"] = model;
+                //}
+                //else
+                //{
+                //    User member = new User();
+                //    member.Login = user.Nickname;
+                //    member.Password = "test".Hash();
+                //    member.ID = Guid.NewGuid();
+                //    Database.Instance.InsertUser(member);
+                //    QzoneOauth oauth = new QzoneOauth()
+                //    {
+                //        AccessTokenKey = qzone2.OAuthTokenKey,
+                //        AccessTokenSecret = qzone2.OAuthTokenSecret,
+                //        OpenId = qzone2.OpenID,
+                //        ID = Guid.NewGuid(),
+                //        UserId = member.ID,
+                //    };
+                //    Database.Instance.InsertQzoneOauth(oauth);
+                //    var newCookie = new HttpCookie("MemberID", member.ID.ToString());
+                //    newCookie.Expires = DateTime.Now.AddDays(10);
+                //    Response.AppendCookie(newCookie);
+                //    Session["QzoneOauth"] = oauth;
+                //}
+                //}
             }
         }
 
@@ -54,19 +116,19 @@ namespace Web
 
         protected void btnAdd_Click(object sender, EventArgs e)
         {
-            model.Name = this.TextBox1.Text.Trim();
-            model.CreatDateTime = DateTime.Now;
-            model.UpdateDateTime = DateTime.Now;
-            model.Status = "1";
-            if (bll.SaveOrUpdateExamPaper(model) == -1)
-            {
-                lbMessage.Text = "添加失败";
-                lbMessage.DataBind();
-            }
-            else
-            {
-                BindGridData();
-            }
+            //model.Name = this.TextBox1.Text.Trim();
+            //model.CreatDateTime = DateTime.Now;
+            //model.UpdateDateTime = DateTime.Now;
+            //model.Status = "1";
+            //if (bll.SaveOrUpdateExamPaper(model) == -1)
+            //{
+            //    lbMessage.Text = "添加失败";
+            //    lbMessage.DataBind();
+            //}
+            //else
+            //{
+            //    BindGridData();
+            //}
         }
     }
 }
