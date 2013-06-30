2feeaeaf13673e9c6d43fe00fb73273c9484612b
wikiwan <wiki.wan@quest.com>
2013-06-30 21:39:51 +0800

    增加配置获取配置信息的一般处理程序

---
+++ Source/Web/Config.ashx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml;
+
+namespace Web
+{
+  /// <summary>
+  /// Config 的摘要说明
+  /// </summary>
+  public class Config : IHttpHandler
+  {
+
+    public void ProcessRequest(HttpContext context)
+    {
+      context.Response.ContentType = "text/plain";
+      string type=context.Request.QueryString["Type"];
+      string name=context.Request.QueryString["Name"];
+      string filePath = context.Server.MapPath("Config.xml");
+      string value = string.Empty;
+      XmlDocument doc = new XmlDocument();
+      doc.Load(filePath);
+      if (!string.IsNullOrEmpty(type))
+      {
+        XmlElement nodeList = doc.ChildNodes (type);
+        for (int i = 0; i < nodeList.ChildNodes.Count; i++)
+        {
+          if (nodeList.ChildNodes[i].Name.Equals(name))
+          {
+            value = nodeList.ChildNodes[i].Value;
+            break;
+          }
+        }
+      }
+      context.Response.Write(value);
+    }
+
+    public bool IsReusable
+    {
+      get
+      {
+        return false;
+      }
+    }
+  }
+}
