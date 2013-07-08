9cb754a043fdad924b943eb9c87b454739b993ca
wgang10 <wgang10@foxmail.com>
2013-07-08 18:52:53 +0800

    添加缓存管理页面

---
+++ Source/Web/CacheManage.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Web
+{
+    public partial class CacheManage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                //Dictionary<string, string> cache = new Dictionary<string, string>();
+                //for (int i = 0; i < Cache.Count; i++)
+                //{
+                    
+                //}
+            }
+        }
+    }
+}
